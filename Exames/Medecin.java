7bb39fa5d3b60f4ddd55e882854c37b5b0f8c405
Vinicius Careli <vcareli@gmail.com>
2026-08-18 10:58:08 -0400

    medpt

--- Exames/Medecin.java
+++ Exames/Medecin.java
@@ -1,10 +1,14 @@
 //Exercice liee Patient.java
+import java.util.*;
+
 public class Medecin {
 	private String nom;
 	private String specialite;
 	private int registre;
+	private ArrayList<Patient> clients;
 
 	public Medecin(String nom, String specialite, int registre) {
+		clients = new ArrayList<>();
 		this.nom = nom;
 		this.specialite = specialite;
 		this.registre = registre;
@@ -26,5 +30,17 @@ public class Medecin {
 
 	public int getRegistre() {
 		return this.registre;
+	}
+	
+	public boolean ajouterPatient(Patient client) {
+		if (client != null) {
+			this.clients.add(client);
+			return true;
+		}
+		return false;
+	}
+	
+	public int sizeClients() {
+		return this.clients.size();
 	}
 }
--- Exames/Patient.java
+++ Exames/Patient.java
@@ -1,4 +1,6 @@
 //Exercice liee Medecin.java
+import java.util.*;
+
 public class Patient {
 	private String nom;
 	private int age;
@@ -42,6 +44,7 @@ public class Patient {
 	}
     
 	public void setMedecin(Medecin medecin) {
+		medecin.ajouterPatient(this);
 		this.medecin = medecin;
 	}
 
@@ -53,20 +56,22 @@ public class Patient {
 		Patient p1 = new Patient("Oceane", 30, 'F');
 		Patient p2 = new Patient("François", 25, 'M');
 		Patient p3 = new Patient("Katherine", 24, 'F');
+		Patient p4 = new Patient("Davi Chatoncio", 18, 'M');
 		Medecin m1 = new Medecin("Dr. House", "INFECTO", 12345);
-		Medecin m2 = new Medecin("Good doctor", 12345);
+		Medecin m2 = new Medecin("Good doctor", 54321);
+		Medecin m3 = new Medecin("Dra. Meredith Grey", "URGENTOLOGUE", 10000);
 
-		System.out.println(p1);
-		if (p1.setAge(15))
+		/*if (p1.setAge(15))
 			System.out.println("Nouvelle age modifié avec succès !");
 		else
-			System.out.println("L'age invalide, patient doivet être adulte");
+			System.out.println("L'age invalide, patient doivet être adulte");*/
 		p1.setMedecin(m2);
 		p2.setMedecin(m1);
-		if (p2.getMedecin() != null)
+		p4.setMedecin(m1);
+		/*if (p2.getMedecin() != null)
 			System.out.println(p2.getMedecin().getNom());
 		else
-			System.out.println("Pt n'a pas de médecin!");
+			System.out.println("Pt n'a pas de médecin!");*/
 		System.out.println("=======================");
 		System.out.println(p1);
 		System.out.println("=======================");
@@ -74,5 +79,11 @@ public class Patient {
 		System.out.println("=======================");
 		System.out.println(p3);
 		System.out.println("=======================");
+		System.out.println(p4);
+		p3.setMedecin(m2);
+		System.out.println("=======================");
+		System.out.println(p3);
+		System.out.println("=======================");
+		//System.out.println("Total patients de " + m1.getNom() + m1.sizeClients());
 	}
 }
